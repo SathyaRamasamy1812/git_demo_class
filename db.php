6c99c1662797257a57be0b38d7e192e0644242c9
Sathya <sathyaramasamy1812@gmail.com>
2024-08-14 06:10:21 +0000

    updated db changes

--- db.php
+++ db.php
@@ -1,1 +1,8 @@
+<<<<<<< HEAD
 trying to push feature_2 to the central repo
+=======
+database_name: test_db
+database_user: test_user
+database_pass: test_pass
+database_rebase: test_rebase
+>>>>>>> a82d879 (updated db)
